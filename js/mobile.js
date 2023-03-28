0f08cdbe79c25b4914900d5a4ac5ad5379971c07
Ismail826 <user@gmail.com>
2023-03-28 16:52:46 +0600

    updated code

--- js/mobile.js
+++ js/mobile.js
@@ -1,40 +1,29 @@
 console.log('js connected')
-
   const searchMobile =()=>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
     // toggleSpinner('block');
     // toggleSearchResults('none');
     searchMobileDetails(searchText);
-    
-   
     document.getElementById('search-field').value='';
     // console.log(searchField);
-   
-
 }
 // spineer
-
 const toggleSpinner =(displayStyle)=>{
   document.getElementById('spineer').style.display = displayStyle;
   }
   const toggleSearchResults =(displayStyle)=>{
   document.getElementById('mobile-result').style.display = displayStyle;
   }
-
   // data fetch
-
-  const searchMobileDetails =(searchText)=>{
+const searchMobileDetails =(searchText)=>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
     `;
      fetch(url)
      .then(res =>  res.json())
      .then(data => showMobile(data.data));
 }
-
 searchMobileDetails("Apple");
-
-
 
 const showMobile =(mobile)=>{
   // console.log(mobile);
@@ -47,52 +36,35 @@ const showMobile =(mobile)=>{
  else{
      noPhone.classList.add('d-none');
  }
-
-  const mobileResult = document.getElementById('mobile-result');
-
+const mobileResult = document.getElementById('mobile-result');
   mobileResult.innerHTML='';
   mobile?.forEach(mobiles =>{
     console.log(mobiles);
-
     const div = document.createElement('div');
     div.classList.add('col')
     div.innerHTML=`<div class="card h-100">
     <img src="${mobiles.image}" class=" w-75 h-75  card-img-top img-fluid" alt="...">
     <div class="card-body">
-      
       <h5 class="card-title">${mobiles.phone_name}</h5>
       <h5 class="card-title">${mobiles.brand}</h5>
-     
-    </div>
+     </div>
     <div class="text-center text-success">
-   
-    <button onclick="loadMobileDetail('${mobiles.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
-    <br>
+   <button onclick="loadMobileDetail('${mobiles.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
     </div>
     `;
     mobileResult.appendChild(div);
-
-   
-    
   });
 //  toggleSpinner('none');
 //     toggleSearchResults('block');
 }
-
- 
-
-
 const loadMobileDetail = id =>{
-  console.log('button is ok boss')
+  console.log('button is ok!')
   console.log(id);
-
-  const url =`https://openapi.programming-hero.com/api/phone/${id}`;
-
-  fetch(url)
+const url =`https://openapi.programming-hero.com/api/phone/${id}`;
+   fetch(url)
   .then(res => res.json())
   .then(data =>loadMobileDetailDisplay(data.data));
 }
-
 const loadMobileDetailDisplay =(details)=>{
   console.log(details);
   const modalTitle = document.getElementById('phoneDetailModalLabel');
@@ -100,9 +72,7 @@ const loadMobileDetailDisplay =(details)=>{
 
   const mobileDetails = document.getElementById('mobile-details');
   mobileDetails.innerHTML='';
- 
-
-    const div = document.createElement('div');
+  const div = document.createElement('div');
     div.classList.add('col')
     div.innerHTML=`
     <img src="${details.image}" class="w-50 h-50 card-img-top img-fluid" alt=".....">
